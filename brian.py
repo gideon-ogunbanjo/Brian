2ec6bc38ef78968a97fb9bcca33869c0d12f274d
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-11 14:27:30 +0100

    defined a class for BMI calculation

--- brian.py
+++ brian.py
@@ -1,3 +1,8 @@
 # importing libraries
 import streamlit as st
 
+# Class for BMI calculation
+class Calculation:
+    def BMICalculator(self, Height, Mass):
+        # Calculate BMI using the formula: BMI = Mass / (Height * Height)
+        BMI = Mass / (Height * Height)
